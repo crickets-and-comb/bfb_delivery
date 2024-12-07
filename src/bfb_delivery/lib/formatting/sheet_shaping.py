95965ce00b161de69b316df7da70bbd1cea03637
Kaleb Coberly <kalebcoberly@gmail.com>
2024-12-07 11:32:27 -0800

    Split by n_books.

--- src/bfb_delivery/lib/formatting/sheet_shaping.py
+++ src/bfb_delivery/lib/formatting/sheet_shaping.py
@@ -7,17 +7,21 @@ import pandas as pd
 from typeguard import typechecked
 
 
-# TODO: Allow to set n sheets.
 # TODO: Make column constants.
+# TODO: Find out what columns we need to keep.
 # TODO: Use Pandera.
+# TODO: Get/make some realish data to test with.
 @typechecked
 def split_chunked_route(
-    sheet_path: Path | str, output_dir: Path | str = "", output_filename: str = ""
-) -> Path:
+    sheet_path: Path | str,
+    output_dir: Path | str = "",
+    output_filename: str = "",
+    n_books: int = 4,
+) -> list[Path]:
     """Split chunked route sheet into separate sheets by driver.
 
     Reads a spreadsheet with stops grouped by driver and splits it into separate sheets.
-    Writes a new Excel workbook with each sheet containing the stops for a single driver.
+    Writes `n_books` Excel workbooks with each sheet containing the stops for a single driver.
     Writes adjacent to the original workbook.
 
     Args:
@@ -26,21 +30,43 @@ def split_chunked_route(
             Empty string (default) saves to the input `sheet_path` directory.
         output_filename: Name of the output workbook.
             Empty string (default) sets filename to "chunked_workbook_split.xlsx".
+        n_books: Number of workbooks to split into.
 
     Returns:
-        Path to the split chunked route workbook.
+        Paths to the split chunked route workbooks.
     """
+    chunked_workbook_split_paths: list[Path] = []
+
+    if n_books <= 0:
+        raise ValueError("n_books must be greater than 0.")
+
     chunked_sheet: pd.DataFrame = pd.read_excel(sheet_path)
 
+    drivers = chunked_sheet["driver"].unique()
+    driver_count = len(drivers)
+    if driver_count < n_books:
+        raise ValueError(
+            "n_books must be less than or equal to the number of drivers: "
+            f"driver_count: {driver_count}, n_books: {n_books}."
+        )
+
     output_dir = Path(output_dir) if output_dir else Path(sheet_path).parent
-    if output_filename == "":
-        # Add date to filename.
-        output_filename = f"chunked_workbook_split_{datetime.now().strftime('%Y%m%d')}.xlsx"
+    base_output_filename = (
+        f"chunked_workbook_split_{datetime.now().strftime('%Y%m%d')}.xlsx"
+        if output_filename == ""
+        else output_filename
+    )
+
     output_dir.mkdir(parents=True, exist_ok=True)
-    chunked_workbook_split_path: Path = output_dir / output_filename
 
-    with pd.ExcelWriter(chunked_workbook_split_path) as writer:
-        for driver, data in chunked_sheet.groupby("driver"):
-            data.to_excel(writer, sheet_name=str(driver), index=False)
+    driver_sets = [drivers[i::n_books] for i in range(n_books)]
+    for i, driver_set in enumerate(driver_sets):
+        i_file_name = f"{base_output_filename.split('.')[0]}_{i + 1}.xlsx"
+        chunked_workbook_split_path: Path = output_dir / i_file_name
+        chunked_workbook_split_paths.append(chunked_workbook_split_path)
+        with pd.ExcelWriter(chunked_workbook_split_path) as writer:
+            driver_set_df = chunked_sheet[chunked_sheet["driver"].isin(driver_set)]
+            for driver, data in driver_set_df.groupby("driver"):
+                data.to_excel(writer, sheet_name=str(driver), index=False)
 
-    return chunked_workbook_split_path
+    return chunked_workbook_split_paths
--- tests/unit/test_sheet_shaping.py
+++ tests/unit/test_sheet_shaping.py
@@ -10,6 +10,7 @@ from bfb_delivery.lib.formatting.sheet_shaping import split_chunked_route
 
 
 # TODO: Can upload multiple CSVs to Circuit instead of Excel file with multiple sheets?
+# TODO: Make all these single-book tests into multibook tests?
 class TestSplitChunkedRoute:
     """Test that split_chunked_route splits a spreadsheet route into sheets by driver."""
 
@@ -24,7 +25,7 @@ class TestSplitChunkedRoute:
         # TODO: Use a fuller mock sheet with all the columns.
         return pd.DataFrame(
             {
-                "driver": ["A", "A", "B", "B", "C", "C"],
+                "driver": ["A", "A", "B", "B", "C", "C", "D"],
                 "address": [
                     "123 Main",
                     "456 Elm",
@@ -32,6 +33,7 @@ class TestSplitChunkedRoute:
                     "1011 Pine",
                     "1213 Maple",
                     "1415 Birch",
+                    "1617 Cedar",
                 ],
             }
         )
@@ -48,50 +50,55 @@ class TestSplitChunkedRoute:
         return fp
 
     @pytest.fixture(scope="class")
-    def mock_chunked_workbook_split_path(
+    def mock_chunked_workbook_split_path_single(
         self, class_tmp_dir: Path, mock_chunked_sheet_raw: pd.DataFrame
     ) -> Path:
-        """A mocked chunked route workbook, split into sheets by driver."""
+        """Path to a mocked chunked route workbook, split into sheets by driver."""
         # For each driver, create a sheet with the driver's data.
-        chunked_workbook_split_path: Path = class_tmp_dir / "mock_chunked_workbook_split.xlsx"
-        with pd.ExcelWriter(chunked_workbook_split_path) as writer:
+        split_workbook_path: Path = class_tmp_dir / "mock_chunked_workbook_split.xlsx"
+        with pd.ExcelWriter(split_workbook_path) as writer:
             for driver, data in mock_chunked_sheet_raw.groupby("driver"):
                 data.to_excel(writer, sheet_name=str(driver), index=False)
 
-        return chunked_workbook_split_path
+        return split_workbook_path
 
     @pytest.fixture(scope="class")
-    def chunked_workbook_split_path(self, mock_chunked_sheet_raw_path: Path) -> Path:
+    def chunked_workbook_split_path_single(self, mock_chunked_sheet_raw_path: Path) -> Path:
         """Get the path to the split chunked route workbook that we are testing."""
-        return split_chunked_route(sheet_path=mock_chunked_sheet_raw_path)
+        return split_chunked_route(sheet_path=mock_chunked_sheet_raw_path, n_books=1)[0]
 
     @pytest.fixture(scope="class")
-    def chunked_workbook_split(self, chunked_workbook_split_path: Path) -> pd.ExcelFile:
+    def chunked_workbook_split_single(
+        self, chunked_workbook_split_path_single: Path
+    ) -> pd.ExcelFile:
         """Get the split chunked route workbook that we are testing."""
-        return pd.ExcelFile(chunked_workbook_split_path)
+        return pd.ExcelFile(chunked_workbook_split_path_single)
 
+    # TODO: Can remove in favor of multi-book contents test.
     def test_by_oracle(
         self,
-        mock_chunked_workbook_split_path: Path,
-        chunked_workbook_split: pd.ExcelFile,
-        chunked_workbook_split_path: Path,
+        mock_chunked_workbook_split_path_single: Path,
+        chunked_workbook_split_single: pd.ExcelFile,
+        chunked_workbook_split_path_single: Path,
     ) -> None:
         """Test that split_chunked_route matches oracle."""
-        for sheet_name in chunked_workbook_split.sheet_names:
+        for sheet_name in chunked_workbook_split_single.sheet_names:
             test_chunked_sheet = pd.read_excel(
-                mock_chunked_workbook_split_path, sheet_name=sheet_name
+                mock_chunked_workbook_split_path_single, sheet_name=sheet_name
             )
             result_chunked_sheet = pd.read_excel(
-                chunked_workbook_split_path, sheet_name=sheet_name
+                chunked_workbook_split_path_single, sheet_name=sheet_name
             )
             pd.testing.assert_frame_equal(test_chunked_sheet, result_chunked_sheet)
 
     def test_sheet_names(
-        self, mock_chunked_sheet_raw: pd.DataFrame, chunked_workbook_split: pd.ExcelFile
+        self,
+        mock_chunked_sheet_raw: pd.DataFrame,
+        chunked_workbook_split_single: pd.ExcelFile,
     ) -> None:
         """The sheet names in the split workbook are the unique drivers in the mock sheet."""
         driver_names = mock_chunked_sheet_raw["driver"].unique()
-        assert set(chunked_workbook_split.sheet_names) == set(driver_names)
+        assert set(chunked_workbook_split_single.sheet_names) == set(driver_names)
 
     @pytest.mark.parametrize("output_dir_type", [Path, str])
     @pytest.mark.parametrize("output_dir", ["", "output"])
@@ -105,21 +112,110 @@ class TestSplitChunkedRoute:
         """Test that the output directory can be set."""
         output_dir = output_dir_type(class_tmp_dir / output_dir)
         output_path = split_chunked_route(
-            sheet_path=mock_chunked_sheet_raw_path, output_dir=output_dir
-        )
+            sheet_path=mock_chunked_sheet_raw_path, output_dir=output_dir, n_books=1
+        )[0]
         assert str(output_path.parent) == str(output_dir)
 
-    @pytest.mark.parametrize("output_filename", ["", "output_filename"])
+    @pytest.mark.parametrize("output_filename", ["", "output_filename.xlsx"])
     def test_set_output_filename(
         self, output_filename: str, mock_chunked_sheet_raw_path: Path
     ) -> None:
         """Test that the output filename can be set."""
         output_path = split_chunked_route(
-            sheet_path=mock_chunked_sheet_raw_path, output_filename=output_filename
-        )
+            sheet_path=mock_chunked_sheet_raw_path, output_filename=output_filename, n_books=1
+        )[0]
         expected_filename = (
-            output_filename
+            f"{output_filename.split(".")[0]}_1.xlsx"
             if output_filename
-            else f"chunked_workbook_split_{datetime.now().strftime('%Y%m%d')}.xlsx"
+            else f"chunked_workbook_split_{datetime.now().strftime('%Y%m%d')}_1.xlsx"
         )
         assert output_path.name == expected_filename
+
+    @pytest.mark.parametrize("n_books", [1, 2, 3])
+    def test_n_books_count(self, n_books: int, mock_chunked_sheet_raw_path: Path) -> None:
+        """Test that the number of workbooks is equal to n_books."""
+        output_paths = split_chunked_route(
+            sheet_path=mock_chunked_sheet_raw_path, n_books=n_books
+        )
+        assert len(output_paths) == n_books
+
+    @pytest.mark.parametrize("n_books", [1, 2, 3])
+    def test_n_books_one_driver_per_sheet(
+        self, n_books: int, mock_chunked_sheet_raw_path: Path
+    ) -> None:
+        """Test that each sheet contains only one driver's data."""
+        output_paths = split_chunked_route(
+            sheet_path=mock_chunked_sheet_raw_path, n_books=n_books
+        )
+
+        driver_sheets = []
+        for output_path in output_paths:
+            workbook = pd.ExcelFile(output_path)
+            driver_sheets = driver_sheets + [
+                pd.read_excel(workbook, sheet_name=sheet) for sheet in workbook.sheet_names
+            ]
+        assert all(sheet["driver"].nunique() == 1 for sheet in driver_sheets)
+
+    @pytest.mark.parametrize("n_books", [1, 2, 3])
+    def test_n_books_unique_drivers(
+        self, n_books: int, mock_chunked_sheet_raw_path: Path
+    ) -> None:
+        """Test that the drivers don't overlap between the split workbooks."""
+        output_paths = split_chunked_route(
+            sheet_path=mock_chunked_sheet_raw_path, n_books=n_books
+        )
+
+        driver_sets = []
+        for output_path in output_paths:
+            driver_sets.append(pd.ExcelFile(output_path).sheet_names)
+        for i, driver_set in enumerate(driver_sets):
+            driver_sets_sans_i = driver_sets[:i] + driver_sets[i + 1 :]  # noqa: E203
+            driver_sets_sans_i = [
+                driver for sublist in driver_sets_sans_i for driver in sublist
+            ]
+            assert len(set(driver_set).intersection(set(driver_sets_sans_i))) == 0
+
+    @pytest.mark.parametrize("n_books", [1, 2, 3])
+    def test_n_books_complete_contents(
+        self, n_books: int, mock_chunked_sheet_raw_path: Path
+    ) -> None:
+        """Test that the input data is all covered in the split workbooks."""
+        output_paths = split_chunked_route(
+            sheet_path=mock_chunked_sheet_raw_path, n_books=n_books
+        )
+
+        full_data = pd.read_excel(mock_chunked_sheet_raw_path)
+
+        driver_sheets = []
+        for output_path in output_paths:
+            workbook = pd.ExcelFile(output_path)
+            driver_sheets = driver_sheets + [
+                pd.read_excel(workbook, sheet_name=sheet) for sheet in workbook.sheet_names
+            ]
+        split_data = pd.concat(driver_sheets, ignore_index=True)
+
+        cols = split_data.columns.to_list()
+        full_data = full_data[cols].sort_values(by=cols).reset_index(drop=True)
+        split_data = split_data.sort_values(by=cols).reset_index(drop=True)
+
+        pd.testing.assert_frame_equal(full_data, split_data)
+
+    @pytest.mark.parametrize("n_books", [0, -1])
+    def test_invalid_n_books(self, n_books: int, mock_chunked_sheet_raw_path: Path) -> None:
+        """Test that an invalid n_books raises a ValueError."""
+        with pytest.raises(ValueError, match="n_books must be greater than 0."):
+            _ = split_chunked_route(sheet_path=mock_chunked_sheet_raw_path, n_books=n_books)
+
+    def test_invalid_n_books_driver_count(self, mock_chunked_sheet_raw_path: Path) -> None:
+        """Test that n_books greater than the number of drivers raises a ValueError."""
+        raw_sheet = pd.read_excel(mock_chunked_sheet_raw_path)
+        driver_count = len(raw_sheet["driver"].unique())
+        n_books = driver_count + 1
+        with pytest.raises(
+            ValueError,
+            match=(
+                f"n_books must be less than or equal to the number of drivers: "
+                f"driver_count: ({driver_count}), n_books: {n_books}."
+            ),
+        ):
+            _ = split_chunked_route(sheet_path=mock_chunked_sheet_raw_path, n_books=n_books)
