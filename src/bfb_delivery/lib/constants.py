a95c584888d4e8f05b759b3601d58b78aef3a766
Kaleb Coberly <kalebcoberly@gmail.com>
2024-12-27 02:00:55 -0800

    Add split columns constants.

--- src/bfb_delivery/lib/constants.py
+++ src/bfb_delivery/lib/constants.py
@@ -6,4 +6,22 @@ from typing import Final
 class Columns:
     """Column name constants."""
 
-    DRIVER: Final[str] = "Driver" # TODO: Accept any case of columns?
+    ADDRESS: Final[str] = "Address"
+    BOX_TYPE: Final[str] = "Box Type"
+    DRIVER: Final[str] = "Driver"  # TODO: Accept any case of columns?
+    EMAIL: Final[str] = "Email"
+    NAME: Final[str] = "Name"
+    NOTES: Final[str] = "Notes"
+    ORDER_COUNT: Final[str] = "Order Count"
+    PHONE: Final[str] = "Phone"
+
+
+SPLIT_ROUTE_COLUMNS: Final[list[str]] = [
+    Columns.NAME,
+    Columns.ADDRESS,
+    Columns.PHONE,
+    Columns.EMAIL,
+    Columns.NOTES,
+    Columns.ORDER_COUNT,
+    Columns.BOX_TYPE,
+]
