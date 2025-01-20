4f8681037c0a44b2e0e21e1513daba8a586073b6
Kaleb Coberly <kalebcoberly@gmail.com>
2025-01-19 20:55:45 -0800

    Uncomment title field.

--- src/bfb_delivery/lib/schema/schema.py
+++ src/bfb_delivery/lib/schema/schema.py
@@ -42,7 +42,7 @@ class CircuitRoutesConcatOut(pa.DataFrameModel):
     notes: Series[str] = pa.Field(coerce=True, nullable=True)
     orderInfo: Series[dict[str, Any]] = pa.Field(coerce=True)
     packageCount: Series[float] = pa.Field(coerce=True, nullable=True, eq=1)
-    # title: Series[str] = pa.Field(coerce=True)
+    title: Series[str] = pa.Field(coerce=True)
 
     # TODO: Use these later in the pipeline when IDs have been extracted from dicts.
     # @pa.check("route", groupby="plan", name="many_to_one_plan_route")
@@ -95,4 +95,70 @@ class CircuitRoutesConcatOut(pa.DataFrameModel):
         )
 
 
-# TODO: Do dict field validations on input to tx.
+# # TODO: Do dict field validations on input to tx.
+# class CircuitRoutesTransformOut(CircuitRoutesConcatOut):
+#     """The schema for the Circuit routes data after transformation."""
+
+#     # route id e.g. "routes/lITTnQsxYffqJQDxIpzr".
+#     route: Series[dict[str, Any]] = pa.Field(coerce=True)
+#     # stop id e.g. "plans/0IWNayD8NEkvD5fQe2SQ/stops/40lmbcQrd32NOfZiiC1b":
+#     id: Series[str] = pa.Field(
+#         coerce=True, unique=True, str_startswith="plans/", str_contains="/stops/"
+#     )
+#     stopPosition: Series[int] = pa.Field(coerce=True, ge=0)
+#     recipient: Series[dict[str, Any]] = pa.Field(coerce=True)
+#     address: Series[dict[str, Any]] = pa.Field(coerce=True)
+#     notes: Series[str] = pa.Field(coerce=True, nullable=True)
+#     orderInfo: Series[dict[str, Any]] = pa.Field(coerce=True)
+#     packageCount: Series[float] = pa.Field(coerce=True, nullable=True, eq=1)
+#     # title: Series[str] = pa.Field(coerce=True)
+
+#     # TODO: Use these later in the pipeline when IDs have been extracted from dicts.
+#     # @pa.check("route", groupby="plan", name="many_to_one_plan_route")
+#     # def check_many_to_one_plan_route(
+#     #     cls, grouped_value: dict[str, Series[str]]  # noqa: B902
+#     # ) -> bool:
+#     #     """Check that each plan has only one route."""
+#     #     # NOTE: route is dict and unhashable, so the reciprocal check is not possible.
+#     #     return all(route.nunique() == 1 for route in grouped_value.values())
+
+#     # @pa.check("plan", groupby="route", name="many_to_one_route_plan")
+#     # def check_many_to_one_route_plan(
+#     #     cls, grouped_value: dict[str, Series[str]]  # noqa: B902
+#     # ) -> bool:
+#     #     """Check that each route has only one plan."""
+#     #     return all(plan.nunique() == 1 for plan in grouped_value.values())
+
+#     # @pa.check("route", groupby="id", name="many_to_one_stop_route")
+#     # def check_many_to_one_stop_route(
+#     #     cls, grouped_value: dict[str, Series[str]]  # noqa: B902
+#     # ) -> bool:
+#     #     """Check that each stop has only one route."""
+#     #     return all(route.nunique() == 1 for route in grouped_value.values())
+
+#     # @pa.check("plan", groupby="id", name="many_to_one_stop_plan")
+#     def check_many_to_one_stop_plan(
+#         cls, grouped_value: dict[str, Series[str]]  # noqa: B902
+#     ) -> bool:
+#         """Check that each stop has only one plan."""
+#         return all(plan.nunique() == 1 for plan in grouped_value.values())
+
+#     @pa.check("stopPosition", groupby="plan", name="stop_position_unique")
+#     def check_stop_position_unique(
+#         cls, grouped_value: dict[str, Series[str]]  # noqa: B902
+#     ) -> bool:
+#         """Check that each plan has unique stop positions."""
+#         return all(
+#             len(stopPosition) == stopPosition.nunique()
+#             for stopPosition in grouped_value.values()
+#         )
+
+#     @pa.check("stopPosition", groupby="plan", name="stop_position_continguous")
+#     def check_stop_position_continguous(
+#         cls, grouped_value: dict[str, Series[str]]  # noqa: B902
+#     ) -> bool:
+#         """Check that each plan has contiguous stop positions."""
+#         return all(
+#             sorted(stopPosition.to_list()) == list(range(0, len(stopPosition.to_list())))
+#             for stopPosition in grouped_value.values()
+#         )
