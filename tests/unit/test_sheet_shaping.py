1558384ae4df8afc55ceef6c727ce3ef2a56984a
Kaleb Coberly <kalebcoberly@gmail.com>
2025-01-27 15:19:40 -0800

    Rescope TestCombineRouteTables test_set_output_dir.

--- tests/unit/test_sheet_shaping.py
+++ tests/unit/test_sheet_shaping.py
@@ -311,182 +311,180 @@ def mock_extra_notes_df() -> pd.DataFrame:
     return extra_notes_df
 
 
+@pytest.fixture(scope="class")
+def mock_chunked_sheet_raw_class_scoped(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    """Save mock chunked route sheet and get path."""
+    tmp_output = tmp_path_factory.mktemp("tmp_output")
+    fp: Path = tmp_output / "mock_chunked_sheet_raw.xlsx"
+    raw_chunked_sheet = pd.DataFrame(
+        columns=SPLIT_ROUTE_COLUMNS + [Columns.DRIVER, Columns.BOX_COUNT, Columns.STOP_NO],
+        data=[
+            (
+                "Recipient One",
+                "123 Main St",
+                "555-555-1234",
+                "Recipient1@email.com",
+                "Notes for Recipient One.",
+                "1",
+                "Basic",
+                "York",
+                "Driver A",
+                2,
+                1,
+            ),
+            (
+                "Recipient Two",
+                "456 Elm St",
+                "555-555-5678",
+                "Recipient2@email.com",
+                "Notes for Recipient Two.",
+                "1",
+                "GF",
+                "Puget",
+                "Driver A",
+                None,
+                2,
+            ),
+            (
+                "Recipient Three",
+                "789 Oak St",
+                "555-555-9101",
+                "Recipient3@email.com",
+                "Notes for Recipient Three.",
+                "1",
+                "Vegan",
+                "Puget",
+                "Driver B",
+                2,
+                3,
+            ),
+            (
+                "Recipient Four",
+                "1011 Pine St",
+                "555-555-1121",
+                "Recipient4@email.com",
+                "Notes for Recipient Four.",
+                "1",
+                "LA",
+                "Puget",
+                "Driver B",
+                None,
+                4,
+            ),
+            (
+                "Recipient Five",
+                "1314 Cedar St",
+                "555-555-3141",
+                "Recipient5@email.com",
+                "Notes for Recipient Five.",
+                "1",
+                "Basic",
+                "Samish",
+                "Driver C",
+                1,
+                5,
+            ),
+            (
+                "Recipient Six",
+                "1516 Fir St",
+                "555-555-5161",
+                "Recipient6@email.com",
+                "Notes for Recipient Six.",
+                "1",
+                "GF",
+                "Sehome",
+                "Driver D #1",
+                1,
+                6,
+            ),
+            (
+                "Recipient Seven",
+                "1718 Spruce St",
+                "555-555-7181",
+                "Recipient7@email.com",
+                "Notes for Recipient Seven.",
+                "1",
+                "Vegan",
+                "Samish",
+                "Driver D #2",
+                2,
+                7,
+            ),
+            (
+                "Recipient Eight",
+                "1920 Maple St",
+                "555-555-9202",
+                "Recipient8@email.com",
+                "Notes for Recipient Eight.",
+                "1",
+                "LA",
+                "South Hill",
+                "Driver D #2",
+                None,
+                8,
+            ),
+            (
+                "Recipient Nine",
+                "2122 Cedar St",
+                "555-555-2223",
+                "Recipient9@email.com",
+                "Notes for Recipient Nine.",
+                "1",
+                "Basic",
+                "South Hill",
+                "Driver E",
+                2,
+                9,
+            ),
+            (
+                "Recipient Ten",
+                "2122 Cedar St",
+                "555-555-2223",
+                "Recipient10@email.com",
+                "Notes for Recipient Ten.",
+                "1",
+                "LA",
+                "South Hill",
+                "Driver E",
+                None,
+                10,
+            ),
+            (
+                "Recipient Eleven",
+                "2346 Ash St",
+                "555-555-2345",
+                "Recipient11@email.com",
+                "Notes for Recipient Eleven.",
+                "1",
+                "Basic",
+                "Eldridge",
+                "Driver F",
+                2,
+                11,
+            ),
+            (
+                "Recipient Twelve",
+                "2122 Cedar St",
+                "555-555-2223",
+                "Recipient12@email.com",
+                "Notes for Recipient Twelve.",
+                "1",
+                "Basic",
+                "Eldridge",
+                "Driver F",
+                None,
+                12,
+            ),
+        ],
+    ).rename(columns={Columns.PRODUCT_TYPE: Columns.BOX_TYPE})
+    raw_chunked_sheet.to_excel(fp, index=False)
+
+    return fp
+
+
 @pytest.mark.usefixtures("mock_is_valid_number")
 class TestSplitChunkedRoute:
     """split_chunked_route splits route spreadsheet into n workbooks with sheets by driver."""
-
-    @pytest.fixture(scope="class")
-    def mock_chunked_sheet_raw_class_scoped(
-        self, tmp_path_factory: pytest.TempPathFactory
-    ) -> Path:
-        """Save mock chunked route sheet and get path."""
-        tmp_output = tmp_path_factory.mktemp("tmp_output")
-        fp: Path = tmp_output / "mock_chunked_sheet_raw.xlsx"
-        raw_chunked_sheet = pd.DataFrame(
-            columns=SPLIT_ROUTE_COLUMNS
-            + [Columns.DRIVER, Columns.BOX_COUNT, Columns.STOP_NO],
-            data=[
-                (
-                    "Recipient One",
-                    "123 Main St",
-                    "555-555-1234",
-                    "Recipient1@email.com",
-                    "Notes for Recipient One.",
-                    "1",
-                    "Basic",
-                    "York",
-                    "Driver A",
-                    2,
-                    1,
-                ),
-                (
-                    "Recipient Two",
-                    "456 Elm St",
-                    "555-555-5678",
-                    "Recipient2@email.com",
-                    "Notes for Recipient Two.",
-                    "1",
-                    "GF",
-                    "Puget",
-                    "Driver A",
-                    None,
-                    2,
-                ),
-                (
-                    "Recipient Three",
-                    "789 Oak St",
-                    "555-555-9101",
-                    "Recipient3@email.com",
-                    "Notes for Recipient Three.",
-                    "1",
-                    "Vegan",
-                    "Puget",
-                    "Driver B",
-                    2,
-                    3,
-                ),
-                (
-                    "Recipient Four",
-                    "1011 Pine St",
-                    "555-555-1121",
-                    "Recipient4@email.com",
-                    "Notes for Recipient Four.",
-                    "1",
-                    "LA",
-                    "Puget",
-                    "Driver B",
-                    None,
-                    4,
-                ),
-                (
-                    "Recipient Five",
-                    "1314 Cedar St",
-                    "555-555-3141",
-                    "Recipient5@email.com",
-                    "Notes for Recipient Five.",
-                    "1",
-                    "Basic",
-                    "Samish",
-                    "Driver C",
-                    1,
-                    5,
-                ),
-                (
-                    "Recipient Six",
-                    "1516 Fir St",
-                    "555-555-5161",
-                    "Recipient6@email.com",
-                    "Notes for Recipient Six.",
-                    "1",
-                    "GF",
-                    "Sehome",
-                    "Driver D #1",
-                    1,
-                    6,
-                ),
-                (
-                    "Recipient Seven",
-                    "1718 Spruce St",
-                    "555-555-7181",
-                    "Recipient7@email.com",
-                    "Notes for Recipient Seven.",
-                    "1",
-                    "Vegan",
-                    "Samish",
-                    "Driver D #2",
-                    2,
-                    7,
-                ),
-                (
-                    "Recipient Eight",
-                    "1920 Maple St",
-                    "555-555-9202",
-                    "Recipient8@email.com",
-                    "Notes for Recipient Eight.",
-                    "1",
-                    "LA",
-                    "South Hill",
-                    "Driver D #2",
-                    None,
-                    8,
-                ),
-                (
-                    "Recipient Nine",
-                    "2122 Cedar St",
-                    "555-555-2223",
-                    "Recipient9@email.com",
-                    "Notes for Recipient Nine.",
-                    "1",
-                    "Basic",
-                    "South Hill",
-                    "Driver E",
-                    2,
-                    9,
-                ),
-                (
-                    "Recipient Ten",
-                    "2122 Cedar St",
-                    "555-555-2223",
-                    "Recipient10@email.com",
-                    "Notes for Recipient Ten.",
-                    "1",
-                    "LA",
-                    "South Hill",
-                    "Driver E",
-                    None,
-                    10,
-                ),
-                (
-                    "Recipient Eleven",
-                    "2346 Ash St",
-                    "555-555-2345",
-                    "Recipient11@email.com",
-                    "Notes for Recipient Eleven.",
-                    "1",
-                    "Basic",
-                    "Eldridge",
-                    "Driver F",
-                    2,
-                    11,
-                ),
-                (
-                    "Recipient Twelve",
-                    "2122 Cedar St",
-                    "555-555-2223",
-                    "Recipient12@email.com",
-                    "Notes for Recipient Twelve.",
-                    "1",
-                    "Basic",
-                    "Eldridge",
-                    "Driver F",
-                    None,
-                    12,
-                ),
-            ],
-        ).rename(columns={Columns.PRODUCT_TYPE: Columns.BOX_TYPE})
-        raw_chunked_sheet.to_excel(fp, index=False)
-
-        return fp
 
     @pytest.mark.parametrize("output_dir_type", [Path, str])
     @pytest.mark.parametrize("output_dir", ["", "dummy_output"])
@@ -845,6 +843,48 @@ class TestSplitChunkedRoute:
                 assert driver_sheet.columns.to_list() == SPLIT_ROUTE_COLUMNS
 
 
+class TestCombineRouteTablesClassScoped:
+    """combine_route_tables combines driver route CSVs into a single workbook."""
+
+    @pytest.fixture(scope="class")
+    def mock_route_tables_class_scoped(
+        self,
+        tmp_path_factory: pytest.TempPathFactory,
+        mock_chunked_sheet_raw_class_scoped: Path,
+    ) -> Path:
+        """Mock the driver route tables returned by Circuit."""
+        tmp_output = tmp_path_factory.mktemp("output")
+        output_dir = tmp_output / "mock_route_tables"
+        output_dir.mkdir()
+
+        output_cols = [Columns.STOP_NO] + SPLIT_ROUTE_COLUMNS
+        chunked_df = pd.read_excel(mock_chunked_sheet_raw_class_scoped)
+        chunked_df.rename(columns={Columns.BOX_TYPE: Columns.PRODUCT_TYPE}, inplace=True)
+        for driver in chunked_df[Columns.DRIVER].unique():
+            output_path = output_dir / f"{MANIFEST_DATE} {driver}.csv"
+            driver_df = chunked_df[chunked_df[Columns.DRIVER] == driver]
+            driver_df[Columns.STOP_NO] = [i + 1 for i in range(len(driver_df))]
+            driver_df[output_cols].to_csv(output_dir / output_path, index=False)
+
+        return output_dir
+
+    @pytest.mark.parametrize("output_dir_type", [Path, str])
+    @pytest.mark.parametrize("output_dir", ["", "dummy_output"])
+    def test_set_output_dir(
+        self,
+        output_dir_type: type[Path | str],
+        output_dir: Path | str,
+        tmp_path: Path,
+        mock_route_tables_class_scoped: Path,
+    ) -> None:
+        """Test that the output directory can be set."""
+        output_dir = output_dir_type(tmp_path / output_dir)
+        output_path = combine_route_tables(
+            input_dir=mock_route_tables_class_scoped, output_dir=output_dir
+        )
+        assert str(output_path.parent) == str(output_dir)
+
+
 class TestCombineRouteTables:
     """combine_route_tables combines driver route CSVs into a single workbook."""
 
@@ -855,20 +895,6 @@ class TestCombineRouteTables:
         output_dir.mkdir()
         output_path = combine_route_tables(input_dir=mock_route_tables, output_dir=output_dir)
         return output_path
-
-    @pytest.mark.parametrize("output_dir_type", [Path, str])
-    @pytest.mark.parametrize("output_dir", ["", "dummy_output"])
-    def test_set_output_dir(
-        self,
-        output_dir_type: type[Path | str],
-        output_dir: Path | str,
-        tmp_path: Path,
-        mock_route_tables: Path,
-    ) -> None:
-        """Test that the output directory can be set."""
-        output_dir = output_dir_type(tmp_path / output_dir)
-        output_path = combine_route_tables(input_dir=mock_route_tables, output_dir=output_dir)
-        assert str(output_path.parent) == str(output_dir)
 
     @pytest.mark.parametrize("output_filename", ["", "dummy_output_filename.csv"])
     def test_set_output_filename(self, output_filename: str, mock_route_tables: Path) -> None:
