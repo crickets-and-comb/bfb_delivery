0a304e9c8941a2f4de6c96d4a17ec4e38ad54f97
Kaleb Coberly <kalebcoberly@gmail.com>
2025-01-29 11:43:15 -0800

    Rescope TestCreateManifests test_driver_cell.

--- tests/unit/test_sheet_shaping.py
+++ tests/unit/test_sheet_shaping.py
@@ -1582,6 +1582,15 @@ class TestCreateManifestsClassScoped:
             ws = basic_manifest_workbook[sheet_name]
             assert ws["A3"].value == f"Date: {MANIFEST_DATE}"
 
+    def test_driver_cell(self, basic_manifest_workbook: Workbook) -> None:
+        """Test that the driver cell is correct."""
+        drivers = [driver.upper() for driver in DRIVERS]
+        for sheet_name in basic_manifest_workbook.sheetnames:
+            ws = basic_manifest_workbook[sheet_name]
+            driver_name = sheet_name.replace(f"{MANIFEST_DATE} ", "")
+            assert ws["A5"].value == f"Driver: {driver_name}"
+            assert driver_name.upper() in drivers
+
 
 # TODO: Revisit moving the rest to class scope once output dirs cconsolidated.
 # Conflicts now.
@@ -1599,15 +1608,6 @@ class TestCreateManifests:
         """Create a basic manifest workbook scoped to class for reuse."""
         workbook = load_workbook(basic_manifest)
         return workbook
-
-    def test_driver_cell(self, basic_manifest_workbook: Workbook) -> None:
-        """Test that the driver cell is correct."""
-        drivers = [driver.upper() for driver in DRIVERS]
-        for sheet_name in basic_manifest_workbook.sheetnames:
-            ws = basic_manifest_workbook[sheet_name]
-            driver_name = sheet_name.replace(f"{MANIFEST_DATE} ", "")
-            assert ws["A5"].value == f"Driver: {driver_name}"
-            assert driver_name.upper() in drivers
 
     def test_agg_cells(
         self, basic_manifest_workbook: Workbook, mock_route_tables: Path
