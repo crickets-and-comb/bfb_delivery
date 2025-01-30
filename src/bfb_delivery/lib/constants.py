2a40fa9ba463075236881d2edc5268f9aebb8a8e
Kaleb Coberly <kalebcoberly@gmail.com>
2025-01-29 23:31:16 -0800

    Use comb_utils.

--- src/bfb_delivery/lib/constants.py
+++ src/bfb_delivery/lib/constants.py
@@ -1,11 +1,10 @@
 """Constants used in the project."""
 
-from dataclasses import dataclass
 from enum import StrEnum
 from typing import Final
 
 import pandas as pd
-from typeguard import typechecked
+from comb_utils import DocString, ErrorDocString
 
 ADDRESS_COLUMN_WIDTH: Final[float] = 40
 
@@ -138,84 +137,6 @@ class Defaults:
         "book_one_drivers_file": "",
         "date": "",
     }
-
-
-@dataclass
-class ErrorDocString:
-    """Error docstrings."""
-
-    type: Final[str]
-    docstring: Final[str]
-
-    @typechecked
-    def __init__(self, type: str, docstring: str) -> None:
-        """Initialize the error docstring."""
-        self.type = type
-        self.docstring = docstring
-
-        return
-
-
-# TODO: Move this to a central utils repo. (And combine with Defaults? Include types?)
-class DocString:
-    """Class to format docstrings for public API `sphinx` docs and CLI `click` help."""
-
-    opening: str = ""
-    args: dict[str, str] = {}
-    raises: list[ErrorDocString] = []
-    returns: list[str] = []
-
-    @typechecked
-    def __init__(
-        self,
-        opening: str,
-        args: dict[str, str],
-        raises: list[ErrorDocString],
-        returns: list[str],
-    ) -> None:
-        """Initialize the docstring parts."""
-        self.opening = opening
-        self.args = args
-        self.raises = raises
-        self.returns = returns
-
-        return
-
-    @property
-    @typechecked
-    def api_docstring(self) -> str:
-        """Format the docstring for sphinx API docs."""
-        parts = [self.opening.strip()]
-
-        if self.args:
-            parts.append("\nArgs:\n")
-            parts.extend([f"  {key}: {value}" for key, value in self.args.items()])
-
-        if self.raises:
-            parts.append("\nRaises:\n")
-            parts.extend([f"  {error.type}: {error.docstring}" for error in self.raises])
-
-        if self.returns:
-            parts.append("\nReturns:\n")
-            parts.extend([f"  {item}" for item in self.returns])
-
-        return "\n\n".join(parts) + "\n"
-
-    @property
-    @typechecked
-    def cli_docstring(self) -> str:
-        """Format the docstring click CLI help."""
-        parts = [self.opening.strip()]
-
-        if self.raises:
-            parts.append("\nRaises:\n")
-            parts.extend([f"  {error.type}: {error.docstring}" for error in self.raises])
-
-        if self.returns:
-            parts.append("\nReturns:\n")
-            parts.extend([f"  {item}" for item in self.returns])
-
-        return "\n\n".join(parts) + "\n"
 
 
 class DocStrings:
