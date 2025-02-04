b856c6b5d0478df9e596a4472b11640bde4f6714
Kaleb Coberly <kalebcoberly@gmail.com>
2025-02-03 16:07:02 -0800

    Better fix to circular import.

--- src/bfb_delivery/lib/dispatch/utils.py
+++ src/bfb_delivery/lib/dispatch/utils.py
@@ -2,17 +2,13 @@
 
 import logging
 import os
-
-# from time import sleep
 from typing import Any
 
 import requests
 from dotenv import load_dotenv
-
-# from requests.auth import HTTPBasicAuth
 from typeguard import typechecked
 
-# from bfb_delivery.lib.constants import RateLimits
+from bfb_delivery.lib.dispatch import api_callers
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -33,21 +29,18 @@ def get_circuit_key() -> str:
 
 # TODO: Pass params instead of forming URL first. ("params", not "json")
 # (Would need to then grab params URL for next page, or just add nextpage to params?)
-# TODO: See if we can make this a api_callers class.
-# Or, just make each call in the loop a new instance.
 @typechecked
 def get_responses(url: str) -> list[dict[str, Any]]:
     """Get all responses from a paginated API endpoint."""
-    # Avoid circular import.
-    from bfb_delivery.lib.dispatch.api_callers import PagedResponseGetter
-
     # Calling the token salsa to trick bandit into ignoring what looks like a hardcoded token.
     next_page_salsa = ""
     next_page_cookie = ""
     responses = []
 
     while next_page_salsa is not None:
-        paged_response_getter = PagedResponseGetter(page_url=url + str(next_page_cookie))
+        paged_response_getter = api_callers.PagedResponseGetter(
+            page_url=url + str(next_page_cookie)
+        )
         paged_response_getter.call_api()
 
         stops = paged_response_getter._response.json()
