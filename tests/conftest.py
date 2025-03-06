eb5de561c205f3d59dc7c8c7d9ff056c9eaee258
Kaleb Coberly <kalebcoberly@gmail.com>
2025-03-06 10:16:08 -0800

    Drop pytest_sessionfinish.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ import pytest
 from typeguard import typechecked
 
 
-def pytest_collection_modifyitems(config: Any, items: list[Any]) -> None:
+def pytest_collection_modifyitems(config: Any, items: list) -> None:
     """Mark test types."""
     unit_tests_dir = os.path.join(config.rootdir, Path("tests/unit"))
     integration_tests_dir = os.path.join(config.rootdir, Path("tests/integration"))
@@ -24,12 +24,6 @@ def pytest_collection_modifyitems(config: Any, items: list[Any]) -> None:
             item.add_marker("integration")
         elif test_path.startswith(e2e_tests_dir):
             item.add_marker("e2e")
-
-
-def pytest_sessionfinish(session: Any, exitstatus: pytest.ExitCode) -> None:
-    """Set as success if no tests are collected."""
-    if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
-        session.exitstatus = pytest.ExitCode.OK
 
 
 @pytest.fixture
