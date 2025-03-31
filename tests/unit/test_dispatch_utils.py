1befd9c815db272284658c0a80f42f56bc6d5990
Kaleb Coberly <kalebcoberly@gmail.com>
2025-03-31 15:22:27 -0700

    Drop debug log.

--- tests/unit/test_dispatch_utils.py
+++ tests/unit/test_dispatch_utils.py
@@ -1,7 +1,5 @@
 """Test for the dispatch utils module."""
 
-from logging import log
-
 from typeguard import typechecked
 
 from bfb_delivery.lib.dispatch.utils import get_circuit_key
@@ -10,5 +8,4 @@ from bfb_delivery.lib.dispatch.utils import get_circuit_key
 @typechecked
 def test_get_circuit_key(mock_key: str) -> None:
     """Test get_circuit_key function."""
-    log(level=0, msg=f"mock_key: {mock_key}; get_circuit_key: {get_circuit_key()}")
     assert get_circuit_key() == mock_key
