e7cea87b5e6bae4c806c1a232a6ce5d6b62b8057
Kaleb Coberly <kalebcoberly@gmail.com>
2025-03-31 15:21:50 -0700

    Add gitattributes with LF eol, and renormalize files.

--- tests/unit/test_dispatch_utils.py
+++ tests/unit/test_dispatch_utils.py
@@ -1,5 +1,7 @@
 """Test for the dispatch utils module."""
 
+from logging import log
+
 from typeguard import typechecked
 
 from bfb_delivery.lib.dispatch.utils import get_circuit_key
@@ -8,4 +10,5 @@ from bfb_delivery.lib.dispatch.utils import get_circuit_key
 @typechecked
 def test_get_circuit_key(mock_key: str) -> None:
     """Test get_circuit_key function."""
+    log(level=0, msg=f"mock_key: {mock_key}; get_circuit_key: {get_circuit_key()}")
     assert get_circuit_key() == mock_key
