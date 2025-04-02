0ad93cb6cf6e6ddaddb26deaabf65aa6106adcec
Kaleb Coberly <kalebcoberly@gmail.com>
2025-04-02 14:48:59 -0700

    Add comment.

--- tests/unit/test_dispatch_utils.py
+++ tests/unit/test_dispatch_utils.py
@@ -8,5 +8,6 @@ from bfb_delivery.lib.dispatch.utils import get_circuit_key
 @typechecked
 def test_get_circuit_key(mock_dispatch_utils_circuit_key: str) -> None:
     """Test get_circuit_key function."""
+    # Seems to be subject to race condition when running `act`. Indeterminant failures.
     circuit_key = get_circuit_key()
     assert circuit_key == mock_dispatch_utils_circuit_key
