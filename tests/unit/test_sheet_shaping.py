ff380fd29cc0b269ce19b91bd4ff862084609f2a
Kaleb Coberly <kalebcoberly@gmail.com>
2026-03-27 17:43:17 -0700

    Correct test notes column index.

--- tests/unit/test_sheet_shaping.py
+++ tests/unit/test_sheet_shaping.py
@@ -908,8 +908,8 @@ class TestFormatCombinedRoutes:
         """Test that the notes column width is correct."""
         for sheet_name in basic_manifest_workbook.sheetnames:
             ws = basic_manifest_workbook[sheet_name]
-            assert ws["F9"].value == Columns.NOTES
-            assert ws.column_dimensions["F"].width == NOTES_COLUMN_WIDTH
+            assert ws["E9"].value == Columns.NOTES
+            assert ws.column_dimensions["E"].width == NOTES_COLUMN_WIDTH
 
     @pytest.mark.parametrize(
         "cell",
