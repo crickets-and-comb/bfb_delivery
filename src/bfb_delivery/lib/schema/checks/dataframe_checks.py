2a377f17a29205318c13c0fc78cf38b638afa8e2
Kaleb Coberly <kalebcoberly@gmail.com>
2026-06-23 20:22:33 -0700

    Swap in field_not_null_where_product.

--- src/bfb_delivery/lib/schema/checks/dataframe_checks.py
+++ src/bfb_delivery/lib/schema/checks/dataframe_checks.py
@@ -1,6 +1,7 @@
 """DataFrame checks."""
 
 import pandas as pd
+import numpy as np
 import pandera.extensions as extensions
 
 from bfb_delivery.lib.constants import CircuitColumns, Columns, IntermediateColumns
@@ -83,10 +84,11 @@ def contiguous_group(
     )
 
 
-@extensions.register_check_method(statistics=["col", "stop_col"])
-def field_not_null_except_depot(df: pd.DataFrame, col: str, stop_col: str) -> bool:
-    """Assert field values are not null except at the depot."""
-    return all(df[col].notnull() | (df[stop_col] == 0))
+@extensions.register_check_method(statistics=["col"])
+def field_not_null_where_product(df: pd.DataFrame, col: str) -> bool:
+    """Assert field values are not null where there is a product to deliver."""
+    no_product_mask = np.array([True if orderInfo.get(CircuitColumns.PRODUCTS) else False for orderInfo in df[CircuitColumns.ORDER_INFO]])
+    return all(df[col].notnull() | no_product_mask)
 
 
 @extensions.register_check_method(statistics=["col_a", "col_b"])
--- src/bfb_delivery/lib/schema/schema.py
+++ src/bfb_delivery/lib/schema/schema.py
@@ -153,9 +153,8 @@ class CircuitRoutesTransformInFromDict(pa.DataFrameModel):
         # https://github.com/crickets-and-comb/bfb_delivery/issues/84
         address1_in_address = True
         address2_in_address = True
-        field_not_null_except_depot = {
-            "col": CircuitColumns.CUSTOM_PROPERTIES,
-            "stop_col": CircuitColumns.STOP_POSITION,
+        field_not_null_where_product = {
+            "col": CircuitColumns.CUSTOM_PROPERTIES
         }
 
 
@@ -222,9 +221,8 @@ class CircuitRoutesTransformOut(pa.DataFrameModel):
             "start_idx": 1,
         }
         increasing_by = {"cols": [IntermediateColumns.DRIVER_SHEET_NAME, Columns.STOP_NO]}
-        field_not_null_except_depot = {
-            "col": Columns.PROTEIN_OPT_IN,
-            "stop_col": Columns.STOP_NO,
+        field_not_null_where_product = {
+            "col": Columns.PROTEIN_OPT_IN
         }
 
 
